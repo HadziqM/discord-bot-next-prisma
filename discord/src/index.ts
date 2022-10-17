16455219713733a415ee72e8331773f37395b15a
HadziqM <dimascrazz@gmail.com>
2022-10-17 23:30:04 +0700

    idk

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -13,7 +13,6 @@ client.slashCommands = new Collection<string, SlashCommand>()
 client.commands = new Collection<string, Command>()
 client.cooldowns = new Collection<string, number>()
 
-
 const handlersDir = join(__dirname, "./handlers")
 readdirSync(handlersDir).forEach(handler => {
     require(`${handlersDir}/${handler}`)(client)
--- discord/src/slashCommands/bind.ts
+++ discord/src/slashCommands/bind.ts
@@ -1,26 +1,10 @@
 import { SlashCommandBuilder,ButtonBuilder, ActionRowBuilder, ButtonStyle } from "discord.js";
 import { SlashCommand } from "../types";
 import {PrismaClient} from "@prisma/client";
-import { Interaction } from "discord.js";
 import Decrypt from '../lib/crypto/decrypt'
 import Embed from '../lib/char_embbed'
-import Bind from '../lib/bind'
-import client from '../index'
 
 const prisma = new PrismaClient();
-const row1:any = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-            .setCustomId('male')
-            .setLabel('Register as Male')
-            .setStyle(ButtonStyle.Primary),
-			)
-        row1.addComponents(
-            new ButtonBuilder()
-                .setCustomId('female')
-                .setLabel('Register as Female')
-                .setStyle(ButtonStyle.Secondary),
-                );
 const command : SlashCommand = {
     command: new SlashCommandBuilder()
     .setName("bind")
@@ -38,15 +22,20 @@ const command : SlashCommand = {
             interaction.deferReply({ephemeral:true})
             const character = await prisma.characters.findFirst({where:{user_id:user.id}}).catch(e=>console.log(e))
             const embed  = await Embed(Number(character?.id))
+            const row1:any = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                    .setCustomId(`malele${character?.id}`)
+                    .setLabel('Register as Male')
+                    .setStyle(ButtonStyle.Primary),
+                    )
+                row1.addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`female${character?.id}`)
+                        .setLabel('Register as Female')
+                        .setStyle(ButtonStyle.Secondary),
+                        );
             await new Promise(()=>setTimeout(()=>interaction.editReply({components:[row1],embeds:[embed[0]],files:[embed[1]]}).catch(e=>console.log(e)),2000))
-            client.on('interactionCreate', async (interaction:Interaction) =>{
-                if (interaction.isButton()){
-                    if(interaction.user.id != interaction.message.interaction?.user.id){await interaction.reply({content:"this button isnt for you",ephemeral:true}).catch(e=>console.log(e));return}
-                    switch (interaction.customId){
-                        case "male":{await Bind(interaction.user.id,Number(character?.id),true);await interaction.reply({content:"Registered as male",ephemeral:true}).catch(e=>console.log(e));break}
-                        case "female":{await Bind(interaction.user.id,Number(character?.id),false);await interaction.reply({content:"Registered as female",ephemeral:true}).catch(e=>console.log(e));break}
-                }}
-            })
             await new Promise(()=>setTimeout(()=>interaction.editReply({components:[],embeds:[],files:[]}).catch(e=>console.log(e)),20000))
             await prisma.$disconnect()
             return
